e611ff623cb8732f5dd1483eae81a18e119750e6
reseggy <reseggy@gmail.com>
2024-01-17 15:32:53 +0300

    Second

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,20 @@
 const places = document.querySelector('.places');
 const placesList = places.querySelector('.places__list');
 
-function createCards (initialCards, deleteCallback){
+function createCard (data, deleteCallback){
   const cardTemplate = document.querySelector('#card-template').content;
   const card = cardTemplate.querySelector('.places__item').cloneNode(true);
-  card.querySelector('.card__title').textContent = initialCards.name;
-  card.querySelector('.card__image').src = initialCards.link;
-  placesList.append(card);
+  const cardImage = card.querySelector('.card__image');
+  card.querySelector('.card__title').textContent = data.name;
+  cardImage.src = data.link;
+  cardImage.alt = data.name; 
 
   const deleteButton = card.querySelector('.card__delete-button');
   deleteButton.addEventListener('click', function (){
     deleteCallback(card);
   });
 
-  let like = card.querySelector('.card__like-button');
+  const like = card.querySelector('.card__like-button');
   like.addEventListener('click', function (evt){
     evt.target.classList.toggle('card__like-button_is-active');
   });
@@ -26,6 +27,6 @@ function deleteCard (card) {
 };
 
 initialCards.forEach(cardData => {
-  const cardElements = createCards(cardData, deleteCard);
+  const cardElements = createCard(cardData, deleteCard);
   placesList.append(cardElements);
 });
